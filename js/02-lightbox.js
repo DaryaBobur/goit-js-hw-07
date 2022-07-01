98beacb527c8e2a5680afca6a23a1c5f2bc9a461
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-01 23:04:54 +0300

    Додає часткове рішення 2 задачі

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+galleryEl.addEventListener('click', cardGallery)
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    }).join('');
+}
+
+function cardGallery(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+}  
+
+}
+        
+let gallery = new SimpleLightbox('.gallery > a');
+gallery.on('show.simplelightbox');
+
 
 console.log(galleryItems);
